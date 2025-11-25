f10ea921cbc988de3617dca1ff62e8702b12be08
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-25 16:34:28 -0300

    chore: install vite-plugin-svgr and configurate in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 // https://vite.dev/config/
@@ -16,5 +17,6 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    svgr(),
   ],
 });
