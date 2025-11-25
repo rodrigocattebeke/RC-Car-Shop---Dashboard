c99bc5813929d213029a4d6166bb16f8682fceab
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-25 17:51:55 -0300

    feat: add navBar component and add navBar mobile height variable

---
+++ src/components/layout/navBar/NavBar.jsx
@@ -0,0 +1,41 @@
+import styles from "./NavBar.module.css";
+import StoreLogo from "@/assets/svg/store.svg?react";
+import CategoriesLogo from "@/assets/svg/sell.svg?react";
+import ReceiptLogo from "@/assets/svg/receipt.svg?react";
+import AnalyticsLogo from "@/assets/svg/analytics.svg?react";
+import PersonLogo from "@/assets/svg/person.svg?react";
+import { NavLink } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <nav className={`${styles.nav} position-md-relative`}>
+      <ul>
+        <li>
+          <NavLink to={"/"} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
+            <StoreLogo className="icon" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/categorias"} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
+            <CategoriesLogo className="icon" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/compras"} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
+            <ReceiptLogo className="icon" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/analiticas"} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
+            <AnalyticsLogo className="icon" />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={"/cuenta"} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
+            <PersonLogo className="icon" />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
