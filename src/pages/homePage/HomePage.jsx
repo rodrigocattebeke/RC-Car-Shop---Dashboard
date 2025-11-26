42f514ca0d553011219a0e9d109e57402cfb4ca8
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-26 16:47:57 -0300

    feat(HomePage): add filter bar UI

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./HomePage.module.css";
 import { Button } from "@/components/ui/button/Button";
 import { Add } from "@/components/icons/Add";
 import { SearchBar } from "@/components/ui/searchBar/SearchBar";
 import { ProductList } from "./components/productList/ProductList";
+import { FILTERS, FILTERS_LABELS } from "@/constants/filters.js";
 import tempProducts from "@/dev/products.json";
 
 export const HomePage = () => {
+  const [filterSelected, setFilterSelected] = useState(FILTERS.ALL); //ALL is the default filter
+
+  const handleFilter = (filter) => {
+    setFilterSelected(filter);
+  };
+
   return (
     <>
       <section className={styles.headerSection}>
@@ -22,7 +29,17 @@ export const HomePage = () => {
           <SearchBar />
         </div>
       </section>
+
       <section className={styles.productsSection}>
+        <div className={styles.filterBar}>
+          <ul>
+            {Object.values(FILTERS).map((filter, i) => (
+              <li className={`${styles.filterOption} ${filter == filterSelected ? styles.active : ""}`} onClick={() => handleFilter(filter)} key={i}>
+                {FILTERS_LABELS[filter]}
+              </li>
+            ))}
+          </ul>
+        </div>
         <ProductList products={tempProducts} />
       </section>
     </>
