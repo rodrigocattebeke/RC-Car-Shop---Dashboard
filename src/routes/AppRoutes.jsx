af67031c74c6886b6a1c93f76824570df9a1a665
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-27 18:02:42 -0300

    feat(AppRoutes): add product page route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { HomePage } from "@/pages/homePage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "@/constants/routes.js";
+import { ProductPage } from "@/pages/productPage/ProductPage";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.PRODUCT_PAGE} element={<ProductPage />} />
     </Routes>
   );
 };
