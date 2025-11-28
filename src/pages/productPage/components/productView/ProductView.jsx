7d6abb0c38ac1a25dff3b0fc0900a5a905fe0e40
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-28 17:20:00 -0300

    feat(ProductView): add FloatingButton

--- src/pages/productPage/components/productView/ProductView.jsx
+++ src/pages/productPage/components/productView/ProductView.jsx
@@ -1,54 +1,60 @@
 import { formatCurrencyPY } from "@/helpers/formatCurrency";
 import styles from "./ProductView.module.css";
+import { FloatingMenu } from "../floatingMenu/FloatingMenu";
 
 export const ProductView = ({ product }) => {
   return (
-    <section className={styles.productSection}>
-      <section className={`${styles.section} ${styles.imgWrapper}`}>
-        <div className={styles.imgContainer}>
-          <img src={product.imgURL} />
-        </div>
-      </section>
+    <>
+      <section className={styles.productSection}>
+        <section className={`${styles.section} ${styles.imgWrapper}`}>
+          <div className={styles.imgContainer}>
+            <img src={product.imgURL} />
+          </div>
+        </section>
 
-      <section className={`${styles.section} ${styles.productInfo}`}>
-        <p className={`${styles.title} ${styles.productTitle}`}>{product.title}</p>
-        <p className={styles.productPrice}>{formatCurrencyPY(product.price)}</p>
-        <div className={styles.dataContainer}>
-          <p>Categoría:</p>
-          <p>{product.category}</p>
-        </div>
-        <div className={styles.dataContainer}>
-          <p>Stock:</p>
-          <p>{product.stock}</p>
-        </div>
-        <div className={`${styles.productTags} ${styles.dataContainer}`}>
-          <p>Tags:</p>
-          <ul>
-            {product.tags.map((tag, i) => (
-              <li key={i}>{tag.toLowerCase()}</li>
-            ))}
-          </ul>
-        </div>
-      </section>
-
-      <section className={`${styles.section} ${styles.productDescription}`}>
-        <p className={styles.title}>Descripción</p>
-        <p>{product.description}</p>
-      </section>
-
-      <section className={`${styles.section} ${styles.productMetadata}`}>
-        <p className={styles.title}>Metadatos</p>
-        <div className={styles.metadataContainer}>
+        <section className={`${styles.section} ${styles.productInfo}`}>
+          <p className={`${styles.title} ${styles.productTitle}`}>{product.title}</p>
+          <p className={styles.productPrice}>{formatCurrencyPY(product.price)}</p>
           <div className={styles.dataContainer}>
-            <p>Añadido el:</p>
-            <p>123</p>
+            <p>Categoría:</p>
+            <p>{product.category}</p>
           </div>
           <div className={styles.dataContainer}>
-            <p>Actualizado el:</p>
-            <p>123</p>
+            <p>Stock:</p>
+            <p>{product.stock}</p>
           </div>
-        </div>
+          <div className={`${styles.productTags} ${styles.dataContainer}`}>
+            <p>Tags:</p>
+            <ul>
+              {product.tags.map((tag, i) => (
+                <li key={i}>{tag.toLowerCase()}</li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <section className={`${styles.section} ${styles.productDescription}`}>
+          <p className={styles.title}>Descripción</p>
+          <p>{product.description}</p>
+        </section>
+
+        <section className={`${styles.section} ${styles.productMetadata}`}>
+          <p className={styles.title}>Metadatos</p>
+          <div className={styles.metadataContainer}>
+            <div className={styles.dataContainer}>
+              <p>Añadido el:</p>
+              <p>123</p>
+            </div>
+            <div className={styles.dataContainer}>
+              <p>Actualizado el:</p>
+              <p>123</p>
+            </div>
+          </div>
+        </section>
       </section>
-    </section>
+
+      {/* Floating Button */}
+      <FloatingMenu />
+    </>
   );
 };
