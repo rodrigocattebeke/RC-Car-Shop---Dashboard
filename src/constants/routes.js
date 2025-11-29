18d450542c643dd48ecfc8a0213fc2ec6a78b98d
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-29 18:14:42 -0300

    feat(routes): add ADD_PRODUCT route

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,9 +1,12 @@
+const PRODUCT = "/producto";
+
 export const ROUTES = {
   HOME: "/",
   ANALYTICS: "/analiticas",
   CATEGORIES: "/categorias",
   PURCHASES: "/compras",
   ACCOUNT: "/cuenta",
-  PRODUCT: "/producto",
+  PRODUCT,
+  ADD_PRODUCT: `${PRODUCT}/agregar`,
   PRODUCT_PAGE: "/producto/:id",
 };
