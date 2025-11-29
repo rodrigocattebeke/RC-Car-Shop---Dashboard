cc1cdcff5beba2804ad4ee6a05c46e5bc4fb5ae2
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-11-29 17:03:18 -0300

    chore(ProductPage): update import after renaming Header to PageHeader

--- src/pages/productPage/ProductPage.jsx
+++ src/pages/productPage/ProductPage.jsx
@@ -1,11 +1,11 @@
-import { Header } from "@/components/layout/header/Header";
+import { PageHeader } from "@/components/layout/pageHeader/PageHeader";
 import product from "@/dev/product.json";
 import { ProductView } from "./components/productView/ProductView";
 
 export const ProductPage = () => {
   return (
     <>
-      <Header title="Detalles del producto" />
+      <PageHeader title="Detalles del producto" />
       <ProductView product={product} />
     </>
   );
