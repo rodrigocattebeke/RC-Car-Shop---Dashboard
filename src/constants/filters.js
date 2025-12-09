ac7a309dc6646ae911e09db89345493f9820e6ab
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-09 17:15:55 -0300

    feat(filters): add stock and alphabetical filters

--- src/constants/filters.js
+++ src/constants/filters.js
@@ -1,9 +1,21 @@
 export const FILTERS = {
   ALL: "all",
-  RECENT: "recent",
+  NEWEST: "newest",
+  IN_STOCK: "in_stock",
+  LOW_STOCK: "low_stock",
+  OUT_OF_STOCK: "out_of_stock",
+  ACTIVE: "active",
+  INACTIVE: "inactive",
+  ALPHABETICAL: "alphabetical",
 };
 
 export const FILTERS_LABELS = {
-  [FILTERS.ALL]: "Todos los productos",
-  [FILTERS.RECENT]: "Recientes",
+  [FILTERS.ALL]: "Todos",
+  [FILTERS.NEWEST]: "Nuevos",
+  [FILTERS.IN_STOCK]: "Con Stock",
+  [FILTERS.LOW_STOCK]: "Stock Bajo",
+  [FILTERS.OUT_OF_STOCK]: "Sin Stock",
+  [FILTERS.ACTIVE]: "Activos",
+  [FILTERS.INACTIVE]: "Inactivos",
+  [FILTERS.ALPHABETICAL]: "Alfabéticamente",
 };
