8227e856b3cadee8dd3a290e141dde041d123fc0
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-13 15:33:19 -0300

    refactor(NavBar): add small tag with icon label

--- src/components/layout/navBar/NavBar.jsx
+++ src/components/layout/navBar/NavBar.jsx
@@ -2,7 +2,7 @@ import styles from "./NavBar.module.css";
 import StoreLogo from "@/assets/svg/store.svg?react";
 import CategoriesLogo from "@/assets/svg/sell.svg?react";
 import ReceiptLogo from "@/assets/svg/receipt.svg?react";
-import AnalyticsLogo from "@/assets/svg/analytics.svg?react";
+import AddIcon from "@/assets/svg/add.svg?react";
 import PersonLogo from "@/assets/svg/person.svg?react";
 import { ROUTES } from "@/constants/routes.js";
 import { NavLink } from "react-router-dom";
@@ -14,26 +14,31 @@ export const NavBar = () => {
         <li>
           <NavLink to={ROUTES.HOME} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
             <StoreLogo className="icon" />
+            <small>Inicio</small>
           </NavLink>
         </li>
         <li>
           <NavLink to={ROUTES.CATEGORIES} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
             <CategoriesLogo className="icon" />
+            <small>Categorias</small>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={ROUTES.ADD_PRODUCT} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink} ${styles.addIcon} `)}>
+            <AddIcon className="icon" />
+            <small>Agregar</small>
           </NavLink>
         </li>
         <li>
           <NavLink to={ROUTES.PURCHASES} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
             <ReceiptLogo className="icon" />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={ROUTES.ANALYTICS} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
-            <AnalyticsLogo className="icon" />
+            <small>Facturas</small>
           </NavLink>
         </li>
         <li>
           <NavLink to={ROUTES.ACCOUNT} className={({ isActive }) => (isActive ? `${styles.navLink} ${styles.active}` : `${styles.navLink}`)}>
             <PersonLogo className="icon" />
+            <small>Cuenta</small>
           </NavLink>
         </li>
       </ul>
