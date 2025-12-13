9c068cb4d8f2b8970f525309b030f61c4dbe52de
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-13 15:34:41 -0300

    feat(AppRoutes): add categories route with component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import { ROUTES } from "@/constants/routes.js";
 import { ProductPage } from "@/pages/productPage/ProductPage";
 import { AddProductPage } from "@/pages/addProductPage/AddProductPage";
+import { CategoriesPage } from "@/pages/categoriesPage/CategoriesPage";
 
 export const AppRoutes = () => {
   return (
     <Routes>
+      <Route path={ROUTES.ADD_PRODUCT} element={<AddProductPage />} />
+      <Route path={ROUTES.CATEGORIES} element={<CategoriesPage />} />
       <Route path={ROUTES.HOME} element={<HomePage />} />
       <Route path={ROUTES.PRODUCT_PAGE} element={<ProductPage />} />
-      <Route path={ROUTES.ADD_PRODUCT} element={<AddProductPage />} />
     </Routes>
   );
 };
