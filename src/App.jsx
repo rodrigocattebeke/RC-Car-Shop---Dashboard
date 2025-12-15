582072662e62278d37a92cf0a88ae6d332870757
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-15 17:18:46 -0300

    refactor: move layout handling to AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { NavBar } from "./components/layout/navBar/NavBar";
 import { AppRoutes } from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
-      <main className="container-xxl">
-        <AppRoutes />
-      </main>
+      <AppRoutes />
     </>
   );
 }
