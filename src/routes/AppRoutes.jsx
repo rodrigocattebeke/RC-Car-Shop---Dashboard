4b7eea617bc654ee76e34a2aaf1e2e7a35958ec4
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-15 17:14:19 -0300

    feat(AppRoutes): import and use MainLayout and AuthLayout

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,14 +4,25 @@ import { ROUTES } from "@/constants/routes.js";
 import { ProductPage } from "@/pages/productPage/ProductPage";
 import { AddProductPage } from "@/pages/addProductPage/AddProductPage";
 import { CategoriesPage } from "@/pages/categoriesPage/CategoriesPage";
+import { LoginPage } from "@/pages/loginPage/LoginPage";
+import { AuthLayout } from "@/layouts/AuthLayout";
+import { MainLayout } from "@/layouts/MainLayout";
+import { ProtectedRoute } from "@/components/auth/ProtectedRoute";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route path={ROUTES.ADD_PRODUCT} element={<AddProductPage />} />
-      <Route path={ROUTES.CATEGORIES} element={<CategoriesPage />} />
-      <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.PRODUCT_PAGE} element={<ProductPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      </Route>
+      <Route element={<ProtectedRoute />}>
+        <Route element={<MainLayout />}>
+          <Route path={ROUTES.ADD_PRODUCT} element={<AddProductPage />} />
+          <Route path={ROUTES.CATEGORIES} element={<CategoriesPage />} />
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.PRODUCT_PAGE} element={<ProductPage />} />
+        </Route>
+      </Route>
     </Routes>
   );
 };
