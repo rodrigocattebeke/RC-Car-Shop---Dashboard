3607ed1e50d16059914e4ec6af275e55d34bf025
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-16 16:21:03 -0300

    feat(AuthContext): handle user creation

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,18 +1,47 @@
 import { auth } from "@/lib/firebase";
 import { singInWithGoogleService } from "@/services/auth/authService";
+import { createIfNotExists, getUserProfile } from "@/services/user/userService";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { createContext } from "react";
+import { useRouteError } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(undefined);
+  const [authUser, setAuthUser] = useState(undefined);
+  const [userProfile, setUserProfile] = useState(undefined);
+  const [unauthorized, setUnauthorized] = useState(false);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-      setUser(firebaseUser);
+    const unsubscribe = onAuthStateChanged(auth, async (firebaseUser) => {
+      setUnauthorized(false);
+
+      if (!firebaseUser) {
+        setUserProfile(null);
+        setAuthUser(null);
+        setLoading(false);
+        return;
+      }
+
+      // Create user
+      await createIfNotExists(firebaseUser);
+
+      // Get user profile
+
+      const userProfile = await getUserProfile(firebaseUser.uid);
+
+      if (!userProfile) {
+        await signOut(auth);
+        setUnauthorized(true);
+        setLoading(false);
+        return;
+      }
+
+      // Set user if exists
+      setUserProfile(userProfile);
+      setAuthUser(firebaseUser);
       setLoading(false);
     });
 
@@ -24,9 +53,11 @@ export const AuthProvider = ({ children }) => {
   };
 
   const state = {
-    user,
+    authUser,
+    userProfile,
+    isAdmin: userProfile?.role === "admin",
+    unauthorized,
     loading,
-    isAuthenticated: !!user,
     singInWithGoogle,
   };
 
