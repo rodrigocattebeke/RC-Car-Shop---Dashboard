1c5358a9c776d232693b234153ea78fca62af405
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-16 16:25:29 -0300

    feat(ProtectedRoute): handle if user auth

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,13 +1,18 @@
 import { useAuth } from "@/hooks/useAuth";
 import React from "react";
-import { Navigate } from "react-router-dom";
-import { Loader } from "../ui/loader/Loader";
+import { Navigate, Outlet } from "react-router-dom";
+import { Loader } from "@/components/ui/loader/Loader";
+import { UnauthrizedPage } from "@/pages/unauthorizedPage/UnauthrizedPage";
 
-export const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, loading } = useAuth();
+export const ProtectedRoute = () => {
+  const { authUser, unauthorized, loading, userProfile } = useAuth();
 
   if (loading) return <Loader fullScreen={true} />;
+  if (!authUser) return <Navigate to={"/login"} replace />;
 
-  if (!isAuthenticated) return <Navigate to={"/login"} replace />;
-  return children;
+  // Logueado pero sin permisos
+  if (unauthorized || !userProfile) {
+    return <UnauthrizedPage />;
+  }
+  return <Outlet />;
 };
