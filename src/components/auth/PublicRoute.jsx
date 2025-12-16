5317b21f394eac182067f6af1950691abeb5b656
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-16 16:52:34 -0300

    feat(PublicRoute): replace children with Outlet

--- src/components/auth/PublicRoute.jsx
+++ src/components/auth/PublicRoute.jsx
@@ -1,15 +1,14 @@
 import { useAuth } from "@/hooks/useAuth";
 import { Loader } from "@/components/ui/loader/Loader";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-export const PublicRoute = ({ children }) => {
+export const PublicRoute = () => {
   const { authUser, loading } = useAuth();
 
-  console.log("hola");
   if (loading) return <Loader fullScreen />;
   if (authUser) {
     return <Navigate to="/" replace />;
   }
 
-  return children;
+  return <Outlet />;
 };
