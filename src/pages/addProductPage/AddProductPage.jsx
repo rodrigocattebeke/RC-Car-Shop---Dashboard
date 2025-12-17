ee01c9d647d5cb053a58573a4e520e94b20d7b44
Rodrigo Cattebeke <107275449+RodrigoCattebeke@users.noreply.github.com>
2025-12-17 17:31:44 -0300

    feat(AddProductPage): add uploadProduct in onSubmit

--- src/pages/addProductPage/AddProductPage.jsx
+++ src/pages/addProductPage/AddProductPage.jsx
@@ -1,14 +1,20 @@
 import { PageHeader } from "@/components/layout/pageHeader/PageHeader";
 import { ProductForm } from "@/components/productForm/ProductForm";
+import { Loader } from "@/components/ui/loader/Loader";
+import { useProducts } from "@/hooks/useProducts";
 
 export const AddProductPage = () => {
-  const onSubmit = (e) => {
-    console.log(e);
+  const { uploadProduct, loading } = useProducts();
+
+  const onSubmit = async (formData) => {
+    const res = await uploadProduct(formData);
+    return res;
   };
   return (
     <>
       <PageHeader title="Agregar producto" />
       <ProductForm onSubmit={onSubmit} />
+      {loading && <Loader fullScreen={true} backdrop={true} />}
     </>
   );
 };
